5aebc258cf08a7206763967887b2ec8be60e45e8
gauravjat135 <gauraviskgf@gmail.com>
2026-09-22 21:09:24 +0530

    Fix local styling and Netlify configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import netlify from "@netlify/vite-plugin-tanstack-start";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  resolve: {
+    tsconfigPaths: true,
+  },
+
   plugins: [
-    TanStackRouterVite(),
-    tanstackStart(),
-    react(),
-    tsconfigPaths(),
     tailwindcss(),
-    netlify(),
+    tanstackStart(),
+
+    ...(command === "build" ? [netlify()] : []),
+
+    react(),
   ],
-});
+}));
